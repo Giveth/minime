584671db822693b0c03f95faa30452c9df2d20ee
Griff Green <saveoursonics@gmail.com>
2017-01-02 13:57:06 +0630

    changed fallback function to throw for accounts
    Also lowered the gas requirement a lot by getting rid of the isContract call

--- MiniMeToken.sol
+++ MiniMeToken.sol
@@ -523,17 +523,12 @@ contract MiniMeToken is Controlled {
     }
 
     /// @notice The fallback function: If the contract's controller has not been
-    ///  set to 0, then the `proxyPayment` method is called which relays the 
-    ///  ether and creates tokens as described in the token controller contract
+    ///  set to a contract with the `proxyPayment` method then sending ether to
+    ///  this contract will not work, otherwise `proxyPayment` is called which 
+    ///  relays the ether and creates tokens as described in the token controller
     function ()  payable {
-        if (controller == 0) throw;
-        if (isContract(controller)) {
-            if (! TokenController(controller).proxyPayment.value(msg.value)(msg.sender))
+            if (!TokenController(controller).proxyPayment.value(msg.value)(msg.sender))
                 throw;
-        } else {
-            if (! controller.send(msg.value)) throw;
-        }
-    }
 
 
 ////////////////
