dbb6eeec8744a9faf6a3e782282416db4a421536
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-11 23:34:52 +0200

    Factor out controller claims

--- contracts/MiniMeToken.sol
+++ contracts/MiniMeToken.sol
@@ -8,6 +8,7 @@ import './MiniMeTokenFactory.sol';
 import './SnapshotTokenBase.sol';
 import './AllowanceBase.sol';
 import './Helpers.sol';
+import './ControllerClaims.sol';
 
 /*
     Copyright 2016, Jordi Baylina
@@ -45,7 +46,8 @@ contract MiniMeToken is
     ISnapshotToken,
     SnapshotTokenBase,
     AllowanceBase,
-    Controlled
+    Controlled,
+    ControllerClaims
 {
 
     string public name;                //The Token's name: e.g. DigixDAO Tokens
@@ -62,8 +64,6 @@ contract MiniMeToken is
 ////////////////
 // Events
 ////////////////
-
-    event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
 
     event NewCloneToken(address indexed _cloneToken, uint _snapshotBlock);
 
@@ -296,26 +296,6 @@ contract MiniMeToken is
         onlyController
     {
         transfersEnabled = _transfersEnabled;
-    }
-
-//////////
-// Safety Methods
-//////////
-
-    /// @notice This method can be used by the controller to extract mistakenly
-    ///  sent tokens to this contract.
-    /// @param _token The address of the token contract that you want to recover
-    ///  set to 0 in case you want to extract ether.
-    function claimTokens(address _token) onlyController {
-        if (_token == 0x0) {
-            controller.transfer(this.balance);
-            return;
-        }
-
-        MiniMeToken token = MiniMeToken(_token);
-        uint balance = token.balanceOf(this);
-        token.transfer(controller, balance);
-        ClaimedTokens(_token, controller, balance);
     }
 
 }
