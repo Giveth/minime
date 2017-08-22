edcb2cdb721ad2fa97fb80d17c817c9e2789768b
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-22 15:14:40 +0200

    Fix voting contract

--- contracts/Extensions/Vote.sol
+++ contracts/Extensions/Vote.sol
@@ -1,4 +1,6 @@
-import './ISnapshotToken.sol';
+pragma solidity ^0.4.13;
+
+import '../Standards/ISnapshotToken.sol';
 
 // https://en.wikipedia.org/wiki/Comparison_of_electoral_systems
 
@@ -24,17 +26,30 @@ import './ISnapshotToken.sol';
 
 contract Vote {
 
-    ISnapshotToken public constant TOKEN;
-    uint256 public constant SNAPSHOT;
-    string[] public constant CHOICES;
+    ISnapshotToken public TOKEN;
+    uint256 public SNAPSHOT;
+    bytes32[] public CHOICE_HASHES;
 
+    string[] public choices;
     uint256[] totals;
 
-    Vote(ISnapshotToken token, string[] options)
-    {
+    // Note: we use hashes because Solidity currently does not support passing
+    //     string[] as an argument for external functions.
+    function Vote(
+        ISnapshotToken token,
+        bytes32[] choiceHashes
+    ) {
         TOKEN = token;
         SNAPSHOT = token.createSnapshot();
-        OPTIONS = options;
+        CHOICE_HASHES = choiceHashes;
+        choices.length = CHOICE_HASHES.length;
+    }
+
+    function initChoice(uint index, string choice)
+    {
+        require(index < CHOICE_HASHES.length);
+        require(keccak256(choice) == CHOICE_HASHES[index]);
+        choices[index] = choice;
     }
 
     function vote(uint256[] votes)
