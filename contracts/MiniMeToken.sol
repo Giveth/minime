80dcefc3af567d21b4f6ee01d789b4b25c26f53e
sophiii <sophiii@protonmail.com>
2017-10-16 14:43:59 +0100

    Update MiniMeToken.sol
    Since the change of `throw` to `require` should we upgrade the version number inside the contract?

--- contracts/MiniMeToken.sol
+++ contracts/MiniMeToken.sol
@@ -79,7 +79,7 @@ contract MiniMeToken is Controlled {
     string public name;                //The Token's name: e.g. DigixDAO Tokens
     uint8 public decimals;             //Number of decimals of the smallest unit
     string public symbol;              //An identifier: e.g. REP
-    string public version = 'MMT_0.1'; //An arbitrary versioning scheme
+    string public version = 'MMT_0.2'; //An arbitrary versioning scheme
 
 
     /// @dev `Checkpoint` is the structure that attaches a block number to a
