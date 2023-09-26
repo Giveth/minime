ada12df49f69b4a05238fb48cb574e4f775d4230
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2023-09-26 03:07:35 -0300

    remove console from import on tests

--- test/MiniMeToken.t.sol
+++ test/MiniMeToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import { Test, console } from "forge-std/Test.sol";
+import { Test } from "forge-std/Test.sol";
 import { Deploy } from "../script/Deploy.s.sol";
 import { DeploymentConfig } from "../script/DeploymentConfig.s.sol";
 
